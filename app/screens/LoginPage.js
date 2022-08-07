a5da0963bec9716d6155c1531ea3021003a4e39f
bhargav27b <bhargav@sketchbrahma.com>
2022-08-07 23:28:51 +0530

    login page styles update

--- app/screens/LoginPage.js
+++ app/screens/LoginPage.js
@@ -82,7 +82,7 @@ const styles = StyleSheet.create({
         marginBottom: 10
     },
     InputLabel: {
-        fontSize: 18,
+        fontSize: 16,
     },
     signUpText: {
         marginBottom: 100,
@@ -90,8 +90,6 @@ const styles = StyleSheet.create({
     },
     linkText: {
         color: color.blueColor,
-        textDecorationStyle: 'solid',      
-        borderBottomWidth: 1,  
     }
 })
 
